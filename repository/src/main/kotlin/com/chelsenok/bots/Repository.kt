2cc9109940f0d1403acdb002bc15e58ed17bc5ff
chelsenok <chelsenok@mail.ru>
2017-07-26 15:21:40 +0300

    Almost all DB

---
+++ repository/src/main/kotlin/com/chelsenok/bots/Repository.kt
@@ -0,0 +1,6 @@
+package com.chelsenok.bots
+
+import org.springframework.stereotype.Repository
+
+@Repository
+class Repository
---
+++ service/src/main/kotlin/com/chelsenok/bots/Service.kt
@@ -0,0 +1,6 @@
+package com.chelsenok.bots
+
+import org.springframework.stereotype.Service
+
+@Service
+class Service
